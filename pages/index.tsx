f2888307077eac3b8e10b9a83b19b100004abfed
Ashok Raju <deville75@gmail.com>
2020-12-13 21:42:31 -0500

    buncha auth testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import Head from 'next/head'
 import styled from 'styled-components';
+import {
+  signIn, 
+  signOut,
+  useSession
+} from 'next-auth/client'
 
 export default function Home() {
+  const [session, loading] = useSession();
   return (
     <Container>
       <Head>
@@ -10,8 +16,17 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;500;600;700;800&family=Montserrat:wght@400;500;600;700;800;900&display=swap" rel="stylesheet"/>
       </Head>
+      {!session && <>
+        Not signed in <br/>
+        <button onClick={signIn}>Sign in</button>
+      </>}
+      {session && <>
+        Signed in as {session.user.email} <br/>
+        <button onClick={signOut}>Sign out</button>
+      </>}
 
       <span>Mint Registration</span>
+
     </Container>
   )
 }
