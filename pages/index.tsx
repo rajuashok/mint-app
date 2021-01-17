1cd613d1edd93b59c8e6da5400a9b3fdfb67820c
Ashok Raju <deville75@gmail.com>
2021-01-17 09:01:15 -0700

    faasfagds

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       </Head>
 
       {/* LOADING */}
-      {!loading && 
+      {loading && 
       <LoginWrapper>
         <CircularSpinner/>
       </LoginWrapper>}
